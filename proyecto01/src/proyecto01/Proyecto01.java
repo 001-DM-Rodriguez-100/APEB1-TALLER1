63aaa6352ae35d8d78f5f714cadb8a04d1736559
MRodzDirect-Original <mabarca1@utpl.edu.ec>
2023-10-23 20:36:22 -0500

    primer commit

---
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto01;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Proyecto01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("""
+                           Ejemplo b\u00e1sico de Java
+                           Versionado a trav\u00e9s de Git
+                           Manejado desde GitHub""");
+        // TODO code application logic here
+    }
+    
+}
