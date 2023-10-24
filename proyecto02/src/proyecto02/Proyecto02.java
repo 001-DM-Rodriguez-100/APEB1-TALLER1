262b288e161ef66e09da74c6d1c9090a37a347e7
MRodzDirect-Original <mabarca1@utpl.edu.ec>
2023-10-23 20:47:17 -0500

    tercer commit, trabajando con Git y GitHubClassroom

---
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto02;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Proyecto02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("""
+                           Ejemplo dos b\u00e1sico de Java
+                           Versionado a trav\u00e9s de Git
+                           Manejado desde GitHub
+                           Adem\u00e1s se usar GitHubClassroom""");    // TODO code application logic here
+    }
+    
+}
